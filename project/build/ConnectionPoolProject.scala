8ac73562a8119a52c960a69e9b0ed8618fff48f5
James Golick <jamesgolick@gmail.com>
2010-09-19 16:10:23 -0700

    publish the project to my maven repo

--- project/build/ConnectionPoolProject.scala
+++ project/build/ConnectionPoolProject.scala
@@ -1,8 +1,17 @@
 import sbt._
 import Process._
 
-class ConnectionPoolProject(info: ProjectInfo) extends DefaultProject(info) {
-  val specs    = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
-  val mockito  = "org.mockito" % "mockito-all" % "1.8.5"
-  val pool     = "commons-pool" % "commons-pool" % "1.5.4" withSources() intransitive()
+class ConnectionPoolProject(info: ProjectInfo) extends DefaultProject(info) with rsync.RsyncPublishing {
+  val specs     = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
+  val mockito   = "org.mockito" % "mockito-all" % "1.8.5"
+  val pool      = "commons-pool" % "commons-pool" % "1.5.4" withSources() intransitive()
+
+  /**
+   * Include docs and source as build artifacts.
+   */
+  override def packageSrcJar = defaultJarPath("-sources.jar")
+  val sourceArtifact = sbt.Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc, `package`)
+
+  def rsyncRepo = "james@jamesgolick.com:/var/www/repo.jamesgolick.com"
 }
