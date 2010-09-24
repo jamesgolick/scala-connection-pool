86fe7f386ee3b2c5a7c8b0cac49a94bca8645519
James Golick <jamesgolick@gmail.com>
2010-09-24 10:11:52 -0700

    setup gem build

--- project/build/ConnectionPoolProject.scala
+++ project/build/ConnectionPoolProject.scala
@@ -1,10 +1,17 @@
 import sbt._
 import Process._
 
-class ConnectionPoolProject(info: ProjectInfo) extends DefaultProject(info) with rsync.RsyncPublishing {
+class ConnectionPoolProject(info: ProjectInfo) extends DefaultProject(info) with rsync.RsyncPublishing with ruby.GemBuilding {
   val specs     = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
   val mockito   = "org.mockito" % "mockito-all" % "1.8.5"
   val pool      = "commons-pool" % "commons-pool" % "1.5.4" withSources() intransitive()
+
+  /**
+   * Gem build settings.
+   */
+  val gemAuthor             = "James Golick"
+  val gemAuthorEmail        = "jamesgolick@gmail.com"
+  override lazy val gemName = "connection_pool_java"
 
   /**
    * Include docs and source as build artifacts.
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,4 +1,6 @@
 class Plugins(info: sbt.ProjectInfo) extends sbt.PluginDefinition(info) {
-  val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
-  val rsyncSBT = "com.codahale" % "rsync-sbt" % "0.1.1"
+  val codaRepo  = "Coda Hale's Repository" at "http://repo.codahale.com/"
+  val rsyncSBT  = "com.codahale" % "rsync-sbt" % "0.1.1"
+  val jamesRepo = "James Golick's Repository" at "http://repo.jamesgolick.com/"
+  val rubySBT   = "com.protose" % "sbt-ruby" % "1.0.5"
 }
